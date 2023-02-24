c858b8bc76d6a0621bd73adcbdf21b8b4447c964
Vadim Rezapov <vadim.rezapov@mai.ru>
2023-02-24 21:25:58 +0300

    Tasks for the workshop 3

---
+++ Задача19/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Clear();
+Console.WriteLine("Введите пятизначное число");
+int chislo = int.Parse(Console.ReadLine()!);
+if (9999 < chislo & chislo < 100000)
+{
+    if (chislo / 10000 == chislo % 10 & (chislo / 1000) % 10 == (chislo / 10) % 10) Console.WriteLine($"Число {chislo} является полиндромом");
+    else Console.WriteLine($"Число {chislo} не является полиндромом");
+}
+else Console.WriteLine("Введено не пятизначное число");
