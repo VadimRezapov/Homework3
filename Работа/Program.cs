7df84b2af3c3af708f16106d8eeb4ca85a8fb304
Vadim Rezapov <vadim.rezapov@mai.ru>
2023-02-27 21:17:40 +0300

    Задачи 34/36/38

---
+++ Работа/Program.cs
@@ -0,0 +1,16 @@
+﻿// Определить количество цифр в числе.
+
+int CountOfNum(int num)
+{
+int count = 0;
+if (num == 0) count = 1;
+else
+{
+while (num != 0)
+{
+num /= 10;
+count++;
+}
+}
+return count;
+}
