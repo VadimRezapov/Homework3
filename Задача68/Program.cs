c39f732bd38e7bf2f4164c19a9c18880e675d094
Vadim Rezapov <vadim.rezapov@mai.ru>
2023-03-05 10:09:18 +0300

    Добавил задачи 65,66,68

---
+++ Задача68/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу вычисления функции Аккермана с помощью рекурсии.
+
+Console.Clear();
+
+int Ackermann(int M, int N)
+{
+    if(M == 0) return N + 1;
+    else if (N == 0) return Ackermann(M - 1, 1);
+    else return Ackermann(M - 1, Ackermann(M, N - 1));
+
+}
+
+Console.Clear();
+Console.WriteLine("Введите число M: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+Console.WriteLine(Ackermann(M, N));
